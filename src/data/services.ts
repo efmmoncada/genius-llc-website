bf75d4b1432222a38a851554ae7e8f4db6ee97f6
efmmoncada <emmanuel.moncada27@gmail.com>
2024-03-22 00:23:23 -0700

    extract and model patterned data
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

---
+++ src/data/services.ts
@@ -0,0 +1,29 @@
+type Service = {
+  title: string;
+  description: string;
+};
+
+const services: Service[] = [
+  {
+    title: "Hot Shot Deliveries",
+    description:
+      "Critical time frame? We’ll ensure your delivery arrives on-time, every time, no mater your schedule.",
+  },
+  {
+    title: "Courier Services",
+    description:
+      "Point to point delivery of anything we can haul. Weather it’s a warehouse or a residential home, we have you covered.",
+  },
+  {
+    title: "Home to Home",
+    description:
+      "Moving but don’t have a way to haul all your stuff? We can deliver your prepackaged boxes and furniture to your new home. ",
+  },
+  {
+    title: "Available 24/7",
+    description:
+      "No matter your time frame, we can ensure efficient delivery any time of the day, any day of the week",
+  },
+];
+
+export default services;
---
+++ src/data/testimonials.ts
@@ -0,0 +1,34 @@
+type Testimonial = {
+  logoSrc: string;
+  company: string;
+  quote: string;
+};
+
+const testimonials: Testimonial[] = [
+  {
+    logoSrc: "https://fakeimg.pl/400x400",
+    company: "Company X",
+    quote:
+      "this company was great to work with. My delivery was amazing and I had confidence it would be delivered on time and safely.",
+  },
+  {
+    logoSrc: "https://fakeimg.pl/400x400",
+    company: "Company X",
+    quote:
+      "this company was great to work with. My delivery was amazing and I had confidence it would be delivered on time and safely.",
+  },
+  {
+    logoSrc: "https://fakeimg.pl/400x400",
+    company: "Company X",
+    quote:
+      "this company was great to work with. My delivery was amazing and I had confidence it would be delivered on time and safely.",
+  },
+  {
+    logoSrc: "https://fakeimg.pl/400x400",
+    company: "Company X",
+    quote:
+      "this company was great to work with. My delivery was amazing and I had confidence it would be delivered on time and safely.",
+  },
+];
+
+export default testimonials;
