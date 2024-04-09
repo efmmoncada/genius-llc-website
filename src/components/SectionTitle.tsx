cb31ac1a27bfb2052c77155491f6670e550c4764
efmmoncada <emmanuel.moncada27@gmail.com>
2024-04-09 13:03:52 -0700

    styling
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,13 @@
-export function SectionTitle({ children }: { children: React.ReactNode }) {
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function SectionTitle({ children, className }: Props) {
   return (
-    <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-brand-primary">
+    <h2 className={`text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-brand-primary ${className}`}>
       {children}
     </h2>
   );
 }
+
