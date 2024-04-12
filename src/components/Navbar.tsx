2443b645154e5f41363215b46c6fd76431c0881a
efmmoncada <emmanuel.moncada27@gmail.com>
2024-04-11 18:22:20 -0700

    add mobile navbar menu
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,39 @@
 import {
+  Link,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from "@nextui-org/react";
+import { useCallback, useState } from 'react';
 
 export function Nav({ children }: { children: React.ReactNode }) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const closeMenu = useCallback(() => setIsMobileMenuOpen(false), []);
+
+  const MenuItems = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Services",
+      href: "#services",
+    },
+    {
+      title: "Testimonials",
+      href: "#testimonials",
+    },
+    {
+      title: "Contact Us",
+      href: "#contact",
+    },
+  ];
+
   return (
     <Navbar
       classNames={{
@@ -13,34 +41,34 @@ export function Nav({ children }: { children: React.ReactNode }) {
         brand: "justify-self-start",
       }}
       maxWidth="full"
+      isMenuOpen={isMobileMenuOpen}
+      onMenuOpenChange={setIsMobileMenuOpen}
+      isBordered
     >
       <NavbarContent justify="start">
         <NavbarBrand>
           <a href="/">{children}</a>
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <a href="/" className="text-lg">
-            Home
-          </a>
-        </NavbarItem>
-        <NavbarItem>
-          <a href="#services" className="text-lg">
-            Services
-          </a>
-        </NavbarItem>
-        <NavbarItem>
-          <a href="#testimonials" className="text-lg">
-            Testimonials
-          </a>
-        </NavbarItem>
-        <NavbarItem>
-          <a href="#contact" className="text-lg">
-            Contact
-          </a>
-        </NavbarItem>
+      <NavbarContent justify="end" className='hidden sm:flex'>
+        {MenuItems.map(({ title, href }, i) => (
+          <NavbarItem key={i}>
+            <Link className="text-lg text-black" href={href}>
+              {title}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
+      <NavbarContent justify='end' className='sm:hidden'>
+        <NavbarMenuToggle aria-label={isMobileMenuOpen ? "Close Menu" : "Open Menu"} />
+      </NavbarContent>
+      <NavbarMenu>
+        {MenuItems.map(({ title, href }, i) => (
+          <NavbarMenuItem key={i}>
+            <Link onClick={closeMenu} className='text-black text-lg' href={href}>{title}</Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
